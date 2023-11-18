690aebf5ba0d40b569b40b5bdcaca8a38a36c1d0
thebdtorrents <151401780+thebdtorrents@users.noreply.github.com>
2023-11-19 02:18:38 +0600

    Add files via upload

---
+++ src/data/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+  site: 'thebdtorrents.club',
+  name: 'TheBDtorrents',
+  title: 'A Privet Torrents for every one',
+  description: 'we are comming  ',
+}
---
+++ src/data/socials.ts
@@ -0,0 +1,17 @@
+export const socials = [
+  {
+    title: 'Instagram',
+    icon: 'mdi:instagram',
+    url: 'thebdtorrents.club',
+  },
+  {
+    title: 'Twitter',
+    icon: 'mdi:twitter',
+    url: 'thebdtorrents.club',
+  },
+  {
+    title: 'YouTube',
+    icon: 'mdi:youtube',
+    url: 'thebdtorrents.club',
+  }
+]
